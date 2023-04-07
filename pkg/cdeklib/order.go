4798a1c86a9260f27acf873e2ec3a67b710475b8
Savva <savvax@gmail.com>
2023-04-07 20:20:19 +0600

    Added generalRequest function for requests.

--- pkg/cdeklib/order.go
+++ pkg/cdeklib/order.go
@@ -3,9 +3,6 @@ package cdeklib
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 )
 
 func (c *Client) CreateOrder(addrFrom string, addrTo string, recipient Recipient, packages []Package, tariffCode int) (orderID string, err error) {
@@ -21,14 +18,7 @@ func (c *Client) CreateOrder(addrFrom string, addrTo string, recipient Recipient
 		Address: addrTo,
 	}
 
-	baseURL, err := url.Parse(c.ApiURL)
-	if err != nil {
-		panic(err)
-	}
-
 	endpoint := "v2/orders"
-
-	fullURL := baseURL.ResolveReference(&url.URL{Path: endpoint})
 
 	rqst := OrderRequest{
 		TariffCode:   tariffCode,
@@ -44,37 +34,12 @@ func (c *Client) CreateOrder(addrFrom string, addrTo string, recipient Recipient
 		panic(err)
 	}
 
-	// Создаем http.Request с указанными параметрами
-	req, err := http.NewRequest("POST", fullURL.String(), bytes.NewBuffer(requestBodyBytes))
-	if err != nil {
-		panic(err)
-	}
-
-	// Устанавливаем необходимые заголовки
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+c.Auth.AccessToken)
-
-	// Отправляем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	// Читаем тело ответа
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var responseObject map[string]interface{}
-	err = json.Unmarshal(responseBody, &responseObject)
+	jsonData, err := c.generalRequest("POST", endpoint, bytes.NewBuffer(requestBodyBytes))
 	if err != nil {
 		return "", err
 	}
 
-	uuid := responseObject["entity"].(map[string]interface{})["uuid"].(string)
+	uuid := jsonData["entity"].(map[string]interface{})["uuid"].(string)
 
 	return uuid, nil
 }
